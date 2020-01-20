a6ddc67944e9b7199dfca151aea828d7dc75bf1c
Abdul Jalil <abdul.ashraf.jalil@gmail.com>
2020-01-20 22:55:12 +0500

    asdasd

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,9 +1,21 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-facebookads/www/FacebookAds.js",
+        "id": "cordova-plugin-facebookads.FacebookAds",
+        "pluginId": "cordova-plugin-facebookads",
+        "clobbers": [
+            "window.FacebookAds"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-extension": "1.5.4",
+    "cordova-facebook-audnet-sdk": "4.26.2",
+    "cordova-plugin-facebookads": "4.23.2"
 }
 // BOTTOM OF METADATA
 });
